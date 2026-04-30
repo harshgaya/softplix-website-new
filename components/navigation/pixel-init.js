541df5b1fc09ddc9e7dae445a9ae1481a0711eba
Yout701 <harshkumarworld@gmail.com>
2026-04-30 17:10:41 +0530

    added pixel

--- components/navigation/pixel-init.js
+++ components/navigation/pixel-init.js
@@ -5,8 +5,10 @@ import { useEffect } from "react";
 export default function PixelInit() {
   useEffect(() => {
     import("react-facebook-pixel").then((ReactPixel) => {
-      ReactPixel.default.init("962704139904226");
-      ReactPixel.default.pageView();
+      const pixel = ReactPixel.default;
+
+      pixel.init("962704139904226");
+      pixel.pageView();
     });
   }, []);
 
